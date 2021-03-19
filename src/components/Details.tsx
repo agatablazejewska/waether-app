b96c55dc5bf32c59818b89de13adefddd3bcdb39
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-03-19 16:59:57 +0100

    Gets it's data through context

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+import CurrentWeatherDataContext from '../context/CurrentWeatherDataContext';
 import DetailsTile from './DetailsTile';
 
 import '../styles/details.css';
 
 const Details = () => {
+    const [currentWeatherData] = useContext(CurrentWeatherDataContext);
+    const humidity = currentWeatherData.humidity ? `${currentWeatherData.humidity}%` : "";
+    const wind = currentWeatherData.wind ? `${currentWeatherData.wind} km/h` : "";
+
     return (
         <div className="Details">
-            <DetailsTile detail="Humidity" value="68%" />
-            <DetailsTile detail="Wind" value="9km/h" />
+            <DetailsTile detail="Humidity" value={humidity} />
+            <DetailsTile detail="Wind" value={wind} />
         </div>
     )
 }
