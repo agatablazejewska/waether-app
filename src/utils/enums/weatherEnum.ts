c9043d29a130743e50680bde861f819c59a7e69e
Agata Sokołowska <agata.blazejewska0@gmail.com>
2021-03-22 20:41:14 +0100

    Updated weather enum so it fits with openweather API icons' ids

--- src/utils/enums/weatherEnum.ts
+++ src/utils/enums/weatherEnum.ts
@@ -1,16 +1,12 @@
 enum Weather  {
-    Sunny= 'sunny',
-    Cloudy= 'cloudy',
-    Partially_Cloudy= 'partially_cloudy',
-    Raining= 'raining',
-    Raining_Sunny= 'raining_sunny',
-    Snowing= 'snowing',
-    Snowing_Sunny='snowing_sunny',
-    Thunder= 'thunder',
-    Thunder_Sunny= 'thunder_sunny',
-    Windy = 'windy',
-    Windy_Sunny = 'windy_sunny',
-
+    Sunny= 800,
+    Cloudy= 8,
+    Partially_Cloudy= 801,
+    Raining= 5,
+    Raining_Sunny= 3,
+    Snowing= 6,
+    Thunder= 2,
+    Windy = 7,
 }
 
 export default Weather;
